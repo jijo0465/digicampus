290dbed437f130b13276a0535943190da15b3e8e
91956 <91956@LAPTOP-D1L3AQ4H>
2020-05-19 15:39:34 +0530

    login page rendering..

---
+++ src/main/java/com/monkmind/digicampus/controllers/TextController.java
@@ -0,0 +1,15 @@
+package com.monkmind.digicampus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TextController {
+	@RequestMapping("/submit")
+	public String getHome()
+	{ 
+		return "sample";
+	}
+	
+
+}
