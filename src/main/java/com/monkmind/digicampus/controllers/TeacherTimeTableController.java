7280dd4089c05c649a0453e8101fe592c69a2f27
Ameen <Ameen@DESKTOP-9FC9933>
2020-05-20 15:26:57 +0530

    teachertimecontroller

--- src/main/java/com/monkmind/digicampus/controllers/TeacherTimeTableController.java
+++ src/main/java/com/monkmind/digicampus/controllers/TeacherTimeTableController.java
@@ -3,10 +3,12 @@ package com.monkmind.digicampus.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.monkmind.digicampus.models.Grade;
+import com.monkmind.digicampus.models.StudentTimeTable;
 import com.monkmind.digicampus.models.TeacherAttendance;
 import com.monkmind.digicampus.models.TeacherTimeTable;
 import com.monkmind.digicampus.services.TeacherTimeTableService;
@@ -26,9 +28,28 @@ public String TeacherTimeTableForm(Model model) {
 @PostMapping
 @RequestMapping("/teacher_timetable")
 public String createTimetable(@ModelAttribute TeacherTimeTable teachertimetable ,Model model) {
-    teachertimetableService.save(teachertimetable);
-    return "index";
-
-
+   TeacherTimeTable timeTable= teachertimetableService.save(teachertimetable);
+   model.addAttribute("teachertimetable", timeTable);
+    return "teachertimetabledisplay";
 }
+	@RequestMapping("/d/{id}")
+	public String timetableTeacher(@PathVariable String id,Model model) {
+	System.out.println(id);
+    TeacherTimeTable teacherTimetable = teachertimetableService.getById(new Long(id));
+    model.addAttribute("teacherTimetable",teacherTimetable);
+     return "updateteachertimetable";
+		}
+
+	@PostMapping
+	@RequestMapping("/updateteachertimetable/{id}")
+	public String InsertTeachertimetable(@ModelAttribute TeacherTimeTable teacherTimetable,Model model) {
+	teachertimetableService.save(teacherTimetable);
+    return "index";
+}
+
+
+
+
+
+
 }
--- src/main/java/com/monkmind/digicampus/repositories/TeacherTimeTableRepository.java
+++ src/main/java/com/monkmind/digicampus/repositories/TeacherTimeTableRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.monkmind.digicampus.models.TeacherTimeTable;
 
 public interface TeacherTimeTableRepository extends CrudRepository<TeacherTimeTable, Long> {
-	Optional<TeacherTimeTable> findById(String teachertimetable);
+	Optional<TeacherTimeTable> findById(Long id);
 }
