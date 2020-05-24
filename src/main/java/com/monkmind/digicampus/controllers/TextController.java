be7a4b062dc62a0a93e70be054c9ad600f64ef74
Gopika P B <Gopika P B@DESKTOP-93SOLU7>
2020-05-24 20:24:25 +0530

    committed

--- src/main/java/com/monkmind/digicampus/controllers/TextController.java
+++ src/main/java/com/monkmind/digicampus/controllers/TextController.java
@@ -1,19 +1,23 @@
 package com.monkmind.digicampus.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TextController {
+	@PostMapping
+	
 	@RequestMapping("/submit")
 	public String getHome()
 	{ 
-		return "fragments/forms/addstud";
+		return "fragments/forms/addstud::addstud";
 	}
 	@RequestMapping("/gopika")
 	public String getForm()
 	{
-		return "fragments/forms/teacher";
+		return "mydashboard";
 	}
 }
 
