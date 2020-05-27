4107427e2a2593f09b4c0d0fe4c3d49f9429afb3
Haritha <Haritha@DESKTOP-5HSIFP1>
2020-05-27 16:24:35 +0530

    timetable

--- src/main/java/com/monkmind/digicampus/controllers/TeacherTimeTableController.java
+++ src/main/java/com/monkmind/digicampus/controllers/TeacherTimeTableController.java
@@ -20,57 +20,48 @@ import lombok.AllArgsConstructor;
 @Controller
 @AllArgsConstructor
 public class TeacherTimeTableController {
-private final TeacherTimeTableService teachertimetableService;
+	private final TeacherTimeTableService teachertimetableService;
 
-@RequestMapping("/timetableform")
-public String TeacherTimeTableForm(Model model) {
-    model.addAttribute("teachertimetable", new TeacherTimeTable());
-    return "teachertimetable_form";
-}
-@PostMapping
-@RequestMapping("/teacher_timetable")
-public String createTimetable(@ModelAttribute TeacherTimeTable teachertimetable ,Model model) {
-   TeacherTimeTable timeTable= teachertimetableService.save(teachertimetable);
-   model.addAttribute("teachertimetable", timeTable);
-    return "index";
-}
+	@RequestMapping("/timetableform")
+	public String TeacherTimeTableForm(Model model) {
+		model.addAttribute("teachertimetable", new TeacherTimeTable());
+		return "teachertimetable_form";
+	}
 
-		@RequestMapping("/teachertimetabledisplayform")
-		public String TeacherTimeTableDisplayForm(Model model) {
-			List<TeacherTimeTable> tchrtime=teachertimetableService.findAll();
-		model.addAttribute("tchrtime",tchrtime);
+	@PostMapping("/teacher_timetable")
+	public String createTimetable(@ModelAttribute TeacherTimeTable teachertimetable, Model model) {
+		TeacherTimeTable timeTable = teachertimetableService.save(teachertimetable);
+		model.addAttribute("teachertimetable", timeTable);
+		return "redirect:/";
+	}
+
+	@RequestMapping("/teachertimetabledisplayform")
+	public String TeacherTimeTableDisplayForm(Model model) {
+		List<TeacherTimeTable> tchrtime = teachertimetableService.findAll();
+		model.addAttribute("tchrtime", tchrtime);
 		return "teachertimetabledisplay";
-		}
-
-
-
-
+	}
 
 	@RequestMapping("/d/{id}")
-	public String timetableTeacher(@PathVariable String id,Model model) {
-	System.out.println(id);
-    TeacherTimeTable teacherTimetable = teachertimetableService.getById(new Long(id));
-    model.addAttribute("teacherTimetable",teacherTimetable);
-     return "updateteachertimetable";
-		}
+	public String timetableTeacher(@PathVariable String id, Model model) {
+		System.out.println(id);
+		TeacherTimeTable teacherTimetable = teachertimetableService.getById(new Long(id));
+		model.addAttribute("teacherTimetable", teacherTimetable);
+		return "updateteachertimetable";
+	}
 
 	@PostMapping
 	@RequestMapping("/updateteachertimetable/{id}")
-	public String InsertTeachertimetable(@ModelAttribute TeacherTimeTable teacherTimetable,Model model) {
-	teachertimetableService.save(teacherTimetable);
-    return "index";
-}
-
-	@RequestMapping("/tchrtime/{id}")
-	public String deleteParent(@PathVariable Long id,Model model)
-	{
-		teachertimetableService.delete(id);
-		return "redirect:/";
-		
+	public String InsertTeachertimetable(@ModelAttribute TeacherTimeTable teacherTimetable, Model model) {
+		teachertimetableService.save(teacherTimetable);
+		return "index";
 	}
 
+	@RequestMapping("/tchrtime/{id}")
+	public String deleteParent(@PathVariable Long id, Model model) {
+		teachertimetableService.delete(id);
+		return "redirect:/";
 
-
-
+	}
 
 }
