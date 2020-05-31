e6b5a92fda79ec5d317c6b784a164c79baaf549b
Haritha <Haritha@DESKTOP-5HSIFP1>
2020-05-31 20:36:43 +0530

    text

--- src/main/java/com/monkmind/digicampus/controllers/TextController.java
+++ src/main/java/com/monkmind/digicampus/controllers/TextController.java
@@ -7,41 +7,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TextController {
 	@PostMapping
-	
+
 	@RequestMapping("/submit")
-	public String getHome()
-	{ 
-
-
-
-
-
-
-
-
+	public String getHome() {
 
 		return "fragments/forms/addstud";
 
 	}
+
 	@RequestMapping("/gopika")
-	public String getForm()
-	{
+	public String getForm() {
 		return "mydashboard";
 	}
-	
+
 	@RequestMapping("/display")
-	public String getStud()
-	{
+	public String getStud() {
 		return "fragments/display/studentdisplay";
 	}
+
 	@RequestMapping("/display2")
-	public String getTeach()
-	{
+	public String getTeach() {
 		return "fragments/forms/teachertimetable";
 	}
 }
-
-        
-        
-      
-         
