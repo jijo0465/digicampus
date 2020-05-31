486f11a6684e977c595811ecb1eb783ad2a8f34c
Ameen <Ameen@DESKTOP-9FC9933>
2020-05-31 19:53:54 +0530

    tecaher command

--- src/main/java/com/monkmind/digicampus/repositories/TeacherRepository.java
+++ src/main/java/com/monkmind/digicampus/repositories/TeacherRepository.java
@@ -1,5 +1,7 @@
 package com.monkmind.digicampus.repositories;
 
+import com.monkmind.digicampus.command.RegisterCommand;
+import com.monkmind.digicampus.command.TeacherRegisterCommand;
 import com.monkmind.digicampus.models.Teacher;
 import com.monkmind.digicampus.models.User;
 
@@ -14,6 +16,7 @@ public interface TeacherRepository extends CrudRepository<Teacher,Long> {
 	Optional<Teacher> findById(Long teacher);
 	Optional<Teacher> findByName(Long long1);
 	List<Teacher> findAll();
+	void save(TeacherRegisterCommand teacherRegisterCommand);
   
 	//Optional<Teacher> findTeacherByTeacherId(String teacher);
 }
