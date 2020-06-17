4eacbdeb6c20fbb327bddae1d47188ae8959b303
jijo0465 <jijo0465@gmail.com>
2020-06-17 20:43:34 +0530

    student updated

--- src/main/java/com/monkmind/digicampus/controllers/StudentController.java
+++ src/main/java/com/monkmind/digicampus/controllers/StudentController.java
@@ -26,118 +26,118 @@ public class StudentController {
     */
 
     @RequestMapping("/add_student")
-	public String studentForm(Model model) {
+    public String studentForm(Model model) {
         RegisterCommand registerCommand = new RegisterCommand();
         List<Grade> gradeCommands = gradeService.listAll();
-	    model.addAttribute("command", registerCommand);
-	    model.addAttribute("gradeList", gradeCommands);
-	    return "fragments/dc-components/dc-screen-layout/dc-student-add.html::dc-student-add";
-	}
+        model.addAttribute("command", registerCommand);
+        model.addAttribute("gradeList", gradeCommands);
+        return "fragments/dc-components/dc-screen-layout/dc-student-add.html::dc-student-add";
+    }
 
-	@PostMapping
-	@RequestMapping("/addstudent")
-	public String Save(@ModelAttribute RegisterCommand command, Model model) {
-		long parentid = (long) Math.floor(Math.random() * 9000000L) + 100000L;
-		command.getParentid().setParentId(Long.toString(parentid));
-		studentService.saveRegisterCommand(command);
-	    return "fragments/forms/confirmpage::confirmpage";
-	}
-	
-	@RequestMapping("/studentdisplay")
-	public String studentdisplay(Model model) {
-		List<Student> liststudents=studentService.listAll();
-		List<Grade> gradeCommands = gradeService.listAll();
-		model.addAttribute("gradeList", gradeCommands);
-		model.addAttribute("liststudents",liststudents);
-		return "fragments/dc-components/dc-screen-layout/dc-student-display.html::dc-student-display";
-	}
-	@RequestMapping("/displaystd/{std}")
-	public String viewclasswise(@PathVariable  Grade std ,Model model) {
-		System.out.println(std);
-		List<Student> students=studentService.findByGradeid(std);
-		model.addAttribute("students", students);
-		return "fragments/dc-components/dc-screen-layout/dc-student-list.html::dc-student-list";
-	}
-	@RequestMapping("/edit/student")
-	public String getEditStudentForm(Model model) {
-		return "fragments/dc-components/dc-screen-layout/dc-student-edit::dc-student-edit";
-	}
+    @PostMapping
+    @RequestMapping("/addstudent")
+    public String Save(@ModelAttribute RegisterCommand command, Model model) {
+        long parentid = (long) Math.floor(Math.random() * 9000000L) + 100000L;
+        command.getParentid().setParentId(Long.toString(parentid));
+        studentService.saveRegisterCommand(command);
+        return "fragments/forms/confirmpage::confirmpage";
+    }
 
-	@RequestMapping("/edit/student/{studentid}")
-	public String studentUpdate(@PathVariable String studentid,Model model) {
-		//ModelAndView mav = new ModelAndView("edit_product");
-		System.out.println(studentid);
-		RegisterCommand registerCommand = new RegisterCommand();
-		model.addAttribute("command",registerCommand);
-		List<Grade> gradeCommands = gradeService.listAll();
-		Student studentbyid = studentService.getStudentByStudentId(studentid);
-		model.addAttribute("gradeList", gradeCommands);
-		model.addAttribute("studentbyid",studentbyid);
-		return "fragments/dc-components/dc-screen-layout/dc-student-edit-02.html::dc-student-edit-02";
-	}
-	/*
-	@PostMapping
-	@RequestMapping("/addstudent")
-	public String Save(@ModelAttribute RegisterCommand command, Model model) {
-		long parentid = (long) Math.floor(Math.random() * 9000000L) + 100000L;
-		command.getParentid().setParentId(Long.toString(parentid));
-		studentService.saveRegisterCommand(command);
-	    return "fragments/forms/confirmpage::confirmpage";
-	}*/
-	@PostMapping
-	@RequestMapping("/updatestudent")
-	public String UpdateStudent(@ModelAttribute  Student savedCommand,Model model) {
-		// Student savedCommand=studentService.(command);
-		 studentService.save(savedCommand);
-		// System.out.println(savedCommand.getDateOfBirth());
-	    return "dashboard";
-	}
+    @RequestMapping("/studentdisplay")
+    public String studentdisplay(Model model) {
+        List<Student> liststudents = studentService.listAll();
+        List<Grade> gradeCommands = gradeService.listAll();
+        model.addAttribute("gradeList", gradeCommands);
+        model.addAttribute("liststudents", liststudents);
+        return "fragments/dc-components/dc-screen-layout/dc-student-display.html::dc-student-display";
+    }
 
-	@ResponseBody
-	@RequestMapping("/validateStudentId/{studentId}")
-	public String InsertStudent(@PathVariable  String studentId ,Model model) {
-		System.out.println(studentId);
-		Student s = studentService.getStudentByStudentId(studentId);
-		if(s!=null){
-			return "exists";
-		}else{
-			return "notExists";
-		}
-	}
-	
+    @RequestMapping("/displaystd/{std}")
+    public String viewclasswise(@PathVariable Grade std, Model model) {
+        System.out.println(std);
+        List<Student> students = studentService.findByGradeid(std);
+        model.addAttribute("students", students);
+        return "fragments/dc-components/dc-screen-layout/dc-student-list.html::dc-student-list";
+    }
 
+    @RequestMapping("/edit/student")
+    public String getEditStudentForm(Model model) {
+        return "fragments/dc-components/dc-screen-layout/dc-student-edit::dc-student-edit";
+    }
 
+    @RequestMapping("/edit/student/{studentid}")
+    public String studentUpdate(@PathVariable String studentid, Model model) {
+        //ModelAndView mav = new ModelAndView("edit_product");
+        System.out.println(studentid);
+        RegisterCommand registerCommand = new RegisterCommand();
+        model.addAttribute("command", registerCommand);
+        List<Grade> gradeCommands = gradeService.listAll();
+        Student student, student1 = new Student();
+        student = studentService.getStudentByStudentId(studentid);
+        long id = student.getId();
+        student1 = studentService.findById(id);
+        model.addAttribute("gradeList", gradeCommands);
+        model.addAttribute("studentbyid", student1);
+        return "fragments/dc-components/dc-screen-layout/dc-student-edit-02.html::dc-student-edit-02";
+    }
 
-	@RequestMapping("/delete/{id}")
-	public String deleteStudent(@PathVariable Long id,Model model)
-	{
-		studentService.delete(id);
-		return "mydashboard";
-		
-	}
-	@RequestMapping("/studentUpdate/{studentId}")
-	public String updatestudent(@PathVariable String studentId,Model model) {
-		Student student = studentService.getStudentByStudentId(studentId);
-		if(student!=null){
-			model.addAttribute("response","exists");
-			model.addAttribute("student",student);
+    /*
+    @PostMapping
+    @RequestMapping("/addstudent")
+    public String Save(@ModelAttribute RegisterCommand command, Model model) {
+        long parentid = (long) Math.floor(Math.random() * 9000000L) + 100000L;
+        command.getParentid().setParentId(Long.toString(parentid));
+        studentService.saveRegisterCommand(command);
+        return "fragments/forms/confirmpage::confirmpage";
+    }*/
+    @PostMapping
+    @RequestMapping("updatestudent")
+    public String UpdateStudent(@ModelAttribute RegisterCommand savedCommand, Model model) {
+        // Student savedCommand=studentService.(command);
+        studentService.saveRegisterCommand(savedCommand);
+        // System.out.println(savedCommand.getDateOfBirth());
+        return "dashboard";
+    }
 
-		}else{
-			model.addAttribute("response","failed");
-		}
-		return "fragments/forms/editstud::editstud";
-	}
-	@RequestMapping("/studentupdate")
-	public String updatestudent(Model model) {
-		List<Grade> gradeCommands = gradeService.listAll();
-		model.addAttribute("gradeList", gradeCommands);
-		return "fragments/edit/studentedit::studentedit";
-	}
-
-	
+    @ResponseBody
+    @RequestMapping("/validateStudentId/{studentId}")
+    public String InsertStudent(@PathVariable String studentId, Model model) {
+        System.out.println(studentId);
+        Student s = studentService.getStudentByStudentId(studentId);
+        if (s != null) {
+            return "exists";
+        } else {
+            return "notExists";
+        }
+    }
 
 
-	
+    @RequestMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Long id, Model model) {
+        studentService.delete(id);
+        return "mydashboard";
+
+    }
+
+    @RequestMapping("/studentUpdate/{studentId}")
+    public String updatestudent(@PathVariable String studentId, Model model) {
+        Student student = studentService.getStudentByStudentId(studentId);
+        if (student != null) {
+            model.addAttribute("response", "exists");
+            model.addAttribute("student", student);
+
+        } else {
+            model.addAttribute("response", "failed");
+        }
+        return "fragments/forms/editstud::editstud";
+    }
+
+    @RequestMapping("/studentupdate")
+    public String updatestudent(Model model) {
+        List<Grade> gradeCommands = gradeService.listAll();
+        model.addAttribute("gradeList", gradeCommands);
+        return "fragments/edit/studentedit::studentedit";
+    }
 
 
 }
